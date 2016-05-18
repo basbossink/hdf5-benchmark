7337fdaec583479ee5938cdf7a7dee8d5969d259
Bas Bossink <bas.bossink@gmail.com>
2016-05-18 17:46:03 +0200

    Increase number of samples.

--- bench.py
+++ bench.py
@@ -7,7 +7,7 @@ import math
 with open('bench.csv', 'w') as csvfile:
     bench_writer = csv.writer(csvfile)
     bench_writer.writerow(['NumberOfDataSets', 'AverageTime', 'StandardDeviation'])
-    for i in range(20, 61, 4):
+    for i in range(30, 61, 2):
         numSets = math.floor(math.pow(10, 0.1*i))
         t = timeit.Timer("call(['./a.out', str(%d)])" % numSets, setup="from subprocess import call") 
         data = t.repeat(repeat=5, number=1)
