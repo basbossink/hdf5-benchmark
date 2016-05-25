81872b41b31e5b65f4d5e416950427d1d23b2b71
Bas Bossink <bas.bossink@gmail.com>
2016-05-25 03:12:50 +0200

    Fix spelling error in title.

--- plotReadResults.R
+++ plotReadResults.R
@@ -7,7 +7,7 @@ svg(filename = "read_bench.svg")
 qplot(x,y) + 
   geom_errorbar(aes(x = x, ymin = y - sd, ymax = y + sd), width = 0.25) + 
   geom_smooth(method = 'lm',formula= y ~ x) + 
-  labs(title = "Time to read a random selection of dataset from a HDF5 file", 
+  labs(title = "Time to read a random selection of datasets from a HDF5 file", 
        x = "Number of datasets read from file", 
        y = "Time (s)")
 dev.off()
