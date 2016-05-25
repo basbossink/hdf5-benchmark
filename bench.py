758c548933029fe2ed3dcedfc0f80ca1ce8a214c
Bas Bossink <bas.bossink@gmail.com>
2016-05-25 03:16:37 +0200

    Use numpy linspace iso power series for data points.

--- bench.py
+++ bench.py
@@ -3,14 +3,13 @@ import timeit
 import csv
 import statistics
 import math
-
+import numpy as np
 with open('bench.csv', 'w') as csvfile:
     bench_writer = csv.writer(csvfile)
     bench_writer.writerow(['NumberOfDataSets', 'AverageTime', 'StandardDeviation'])
-    for i in range(30, 61, 2):
-        numSets = math.floor(math.pow(10, 0.1*i))
-        t = timeit.Timer("call(['./a.out', str(%d)])" % numSets, setup="from subprocess import call") 
+    for i in np.round(np.linspace(100, 1e6, 10)):
+        t = timeit.Timer("call(['./a.out', str(%d)])" % i, setup="from subprocess import call") 
         data = t.repeat(repeat=5, number=1)
-        bench_writer.writerow([numSets, statistics.mean(data), statistics.stdev(data)])
+        bench_writer.writerow([i, statistics.mean(data), statistics.stdev(data)])
         
     
